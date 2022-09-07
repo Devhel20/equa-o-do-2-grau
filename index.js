b8ab907178d732d8e4b9a1cc519bc2f904b5e614
Devhel20 <112909420+Devhel20@users.noreply.github.com>
2022-09-07 11:08:17 -0300

    Inicio certo.

---
+++ index.js
@@ -0,0 +1,9 @@
+const botao = document.getElementById ("botao")
+const a = document.getElementById ("a")
+const b = document.getElementById ("b")
+const c = document.getElementById ("c")
+botao.addEventListener ("click", funcao)
+function funcao (){
+    alert ("comunicou ")
+    return
+}
